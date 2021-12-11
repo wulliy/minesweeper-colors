7ad630dd8b9bff173cd1da41930136a234a273db
willy <79392954+wulliy@users.noreply.github.com>
2021-12-11 02:51:56 -0500

    Update colors.js

--- colors.js
+++ colors.js
@@ -79,74 +79,74 @@
 	}
 
 	window.minesweeper.setColors = () => {
-		s_7pd = function(a, b, c) {
-			document.getElementById("NWJp1d").style.backgroundColor = window.minesweeper.colors.NAVBAR_BG_COLOR
-			document.getElementsByClassName("HhuoRb")[0].style.backgroundColor = window.minesweeper.colors.TRY_AGAIN_BG_COLOR
-
-	        switch (c) {
-	        case "GRASS":
-	            a.context.fillStyle = 0 === (b.x + b.y) % 2 ? window.minesweeper.colors.GRASS_DARK : window.minesweeper.colors.GRASS_LIGHT;
-	            s_9pd(a, b);
-	            break;
-	        case "DUG":
-	            a.context.fillStyle = 0 === (b.x + b.y) % 2 ? s_$pd(a, window.minesweeper.colors.DUG_LIGHT, window.minesweeper.colors.WATER_LIGHT, b) : s_$pd(a, window.minesweeper.colors.DUG_DARK, window.minesweeper.colors.WATER_DARK, b);
-	            s_9pd(a, b);
-	            break;
-	        case "ADJACENT":
-	            s_7pd(a, b, "DUG");
-	            c = .62 * a.cellSize;
-	            a.context.font = "bold " + c + "pt Roboto";
-	            a.context.textAlign = "center";
-	            var d = a.oa[b.x][b.y].DI;
-	            a.context.fillStyle = s_$pd(a, s_aqd[d - 1], 0 === (b.x + b.y) % 2 ? "#90CAF9" : "#83C4F7", b);
-	            a.context.fillText(d.toString(), b.x * a.cellSize + a.cellSize / 2, b.y * a.cellSize + c + (a.cellSize - c) / 2.1);
-	            break;
-	        case "FLAG":
-	            s_7pd(a, b, "GRASS");
-	            a.Ps.render(Math.min(Math.floor(a.oa[b.x][b.y].JGa), 9), new s_hg(b.x * a.cellSize,b.y * a.cellSize), new s_hg(0,0), 0, a.cellSize / a.Us.Qd());
-	            break;
-	        case "BAD_FLAG":
-	            s_7pd(a, b, "GRASS");
-	            a.context.drawImage(a.Ht, b.x * a.cellSize, b.y * a.cellSize, a.cellSize, a.cellSize);
-	            break;
-	        case "DETONATED_MINE":
-	            a.context.fillStyle = a.oa[b.x][b.y].color;
-	            s_9pd(a, b);
-	            s_7pd(a, b, "MINE");
-	            break;
-	        case "MINE":
-	            a.context.fillStyle = s_IC(a.oa[b.x][b.y].color, .35),
-	            a.context.beginPath(),
-	            a.context.arc(b.x * a.cellSize + a.cellSize / 2, b.y * a.cellSize + a.cellSize / 2, a.cellSize / 4, 0, 2 * Math.PI, !1),
-	            a.context.fill()
-	        }
-	        a.Aa && a.Aa.equals(b) && (a.context.strokeStyle = "#578A34",
-	        a.context.lineWidth = a.cellSize / 10,
-	        a.context.beginPath(),
-	        a.context.rect(b.x * a.cellSize, b.y * a.cellSize, a.cellSize, a.cellSize),
-	        a.context.stroke(),
-	        a.context.setTransform(1, 0, 0, 1, 0, 0),
-	        null !== a.Ha && (a.context.fillStyle = "#578A34",
-	        a.context.beginPath(),
-	        a.context.arc(a.Ha.x, a.Ha.y, a.wb, 0, 2 * Math.PI, !1),
-	        a.context.fill(),
-	        b = 2.4 * a.wb,
-	        a.context.drawImage(a.Hw, a.Ha.x - b / 2, a.Ha.y - b / 2, b, b)),
-	        null !== a.Ma && (a.context.fillStyle = "#578A34",
-	        a.context.beginPath(),
-	        a.context.arc(a.Ma.x, a.Ma.y, a.wb, 0, 2 * Math.PI, !1),
-	        a.context.fill(),
-	        b = 2.25 * a.wb,
-	        a.context.drawImage(a.ul, a.Ma.x - b / 2.25, a.Ma.y - b / 2, b, b)),
-	        null !== a.Ga && (b = .75 * a.wb,
-	        a.context.fillStyle = "#578A34",
-	        a.context.beginPath(),
-	        a.context.arc(a.Ga.x, a.Ga.y, b, 0, 2 * Math.PI, !1),
-	        a.context.fill(),
-	        b *= 1.5,
-	        a.context.drawImage(a.Tr, a.Ga.x - b / 2, a.Ga.y - b / 2, b, b)),
-	        a.context.setTransform(1, 0, 0, 1, a.Ba.width, a.Ba.height))
-	    }
+		document.getElementById("b11qzc").style.background = window.minesweeper.colors.GRASS_DARK
+		document.getElementsByClassName("HhuoRb")[0].style.background = window.minesweeper.colors.GRASS_DARK
+		
+		s_Vud = function(a, b, c) {
+		    switch (c) {
+			case "GRASS":
+			    a.context.fillStyle = 0 === (b.x + b.y) % 2 ? window.minesweeper.colors.GRASS_DARK : window.minesweeper.colors.GRASS_LIGHT;
+			    s_Xud(a, b);
+			    break;
+			case "DUG":
+			    a.context.fillStyle = 0 === (b.x + b.y) % 2 ? s_Yud(a, window.minesweeper.colors.DUG_LIGHT, window.minesweeper.colors.WATER_LIGHT, b) : s_Yud(a, window.minesweeper.colors.DUG_DARK, window.minesweeper.colors.WATER_DARK, b);
+			    s_Xud(a, b);
+			    break;
+			case "ADJACENT":
+			    s_Vud(a, b, "DUG");
+			    c = .62 * a.cellSize;
+			    a.context.font = "bold " + c + "pt Roboto";
+			    a.context.textAlign = "center";
+			    var d = a.oa[b.x][b.y].yJ;
+			    a.context.fillStyle = s_Yud(a, s_Zud[d - 1], 0 === (b.x + b.y) % 2 ? "#90CAF9" : "#83C4F7", b);
+			    a.context.fillText(d.toString(), b.x * a.cellSize + a.cellSize / 2, b.y * a.cellSize + c + (a.cellSize - c) / 2.1);
+			    break;
+			case "FLAG":
+			    s_Vud(a, b, "GRASS");
+			    a.Es.render(Math.min(Math.floor(a.oa[b.x][b.y].rKa), 9), new s_Og(b.x * a.cellSize, b.y * a.cellSize), new s_Og(0, 0), 0, a.cellSize / a.Es.Dd());
+			    break;
+			case "BAD_FLAG":
+			    s_Vud(a, b, "GRASS");
+			    a.context.drawImage(a.xu, b.x * a.cellSize, b.y * a.cellSize, a.cellSize, a.cellSize);
+			    break;
+			case "DETONATED_MINE":
+			    a.context.fillStyle = a.oa[b.x][b.y].color;
+			    s_Xud(a, b);
+			    s_Vud(a, b, "MINE");
+			    break;
+			case "MINE":
+			    a.context.fillStyle = s_mD(a.oa[b.x][b.y].color, .35),
+				a.context.beginPath(),
+				a.context.arc(b.x * a.cellSize + a.cellSize / 2, b.y * a.cellSize + a.cellSize / 2, a.cellSize / 4, 0, 2 * Math.PI, !1),
+				a.context.fill()
+		    }
+		    a.Aa && a.Aa.equals(b) && (a.context.strokeStyle = "#578A34",
+			a.context.lineWidth = a.cellSize / 10,
+			a.context.beginPath(),
+			a.context.rect(b.x * a.cellSize, b.y * a.cellSize, a.cellSize, a.cellSize),
+			a.context.stroke(),
+			a.context.setTransform(1, 0, 0, 1, 0, 0),
+			null !== a.Ja && (a.context.fillStyle = "#578A34",
+			    a.context.beginPath(),
+			    a.context.arc(a.Ja.x, a.Ja.y, a.yb, 0, 2 * Math.PI, !1),
+			    a.context.fill(),
+			    b = 2.4 * a.yb,
+			    a.context.drawImage(a.Iv, a.Ja.x - b / 2, a.Ja.y - b / 2, b, b)),
+			null !== a.Na && (a.context.fillStyle = "#578A34",
+			    a.context.beginPath(),
+			    a.context.arc(a.Na.x, a.Na.y, a.yb, 0, 2 * Math.PI, !1),
+			    a.context.fill(),
+			    b = 2.25 * a.yb,
+			    a.context.drawImage(a.$k, a.Na.x - b / 2.25, a.Na.y - b / 2, b, b)),
+			null !== a.Ga && (b = .75 * a.yb,
+			    a.context.fillStyle = "#578A34",
+			    a.context.beginPath(),
+			    a.context.arc(a.Ga.x, a.Ga.y, b, 0, 2 * Math.PI, !1),
+			    a.context.fill(),
+			    b *= 1.5,
+			    a.context.drawImage(a.Ds, a.Ga.x - b / 2, a.Ga.y - b / 2, b, b)),
+			a.context.setTransform(1, 0, 0, 1, a.Ba.width, a.Ba.height))
+		}
 
 	    if (!Object.keys(Array.prototype).includes("_push")) {
 	    	Array.prototype._push = Array.prototype.push
